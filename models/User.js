ee58821f45545074a07e58f01da73876e28d349d
sivalakshmik <soniakv.2822@gmail.com>
2025-09-21 11:57:29 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   resetToken: { type: String },
@@ -8,3 +9,4 @@ const userSchema = new mongoose.Schema({
 });
 
 export default mongoose.model("User", userSchema);
+
