3ac28013528c4d0fc15327d283b316e4870c4616
sivalakshmik <soniakv.2822@gmail.com>
2025-09-21 12:18:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,13 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
+  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
